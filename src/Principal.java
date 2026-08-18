025ebe258e70850b0909c08e12b504d534342052
Jeferson_san102 <sanchezjeferson477@gmail.com>
2026-08-17 20:01:35 -0600

    dd

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,8 @@
 import javax.swing.SwingUtilities;
 public class Principal {
+    //Estudiantes:
+    //Jeferson David Sanchez Sanchez
+    //Axel Josue Montoya Ovares
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             DashboardSwing ventana = new DashboardSwing();
